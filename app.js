aa0940a16fde0697743ca220222b20fbfe3a877d
= <krishnajha366@gmail.com>
2020-08-28 23:39:37 +0530

    Basic server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("It works")
+})
+
+PORT =  process.env.PORT || 8080
+
+app.listen(
+    PORT,
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
