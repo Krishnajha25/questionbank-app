8aa2e7ef075edab9d3124cdd22cd8fb7d69f3b99
= <krishnajha366@gmail.com>
2020-08-29 01:41:28 +0530

    app.js file updated

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const semRoute = require('./routes/semester')
+const questionRoute = require('./routes/question')
+const streamRoute = require('./routes/stream')
+const subjectRoute = require('./routes/subject')
+
+
+// Load Config
+dotenv.config({path: './config/.env'})
 
 const app = express()
 
+// Cors config
+app.use(cors())
+
+connectDB()
+
+// Routes
 app.get('/', (req, res) => {
     res.send("It works")
 })
+
+app.use('/semester', semRoute)
+app.use('/question', questionRoute)
+app.use('/stream', streamRoute)
+app.use('/subject', subjectRoute)
+
 
 PORT =  process.env.PORT || 3000
 
